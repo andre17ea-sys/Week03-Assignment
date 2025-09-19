9d0e174ffbd4a28826455df4504bf09703a38f9f
ANDREEA TOMA <andre17ea@gmail.com>
2025-09-19 11:53:50 +0100

    Add upgrade shop with fetch and buy buttons

--- app.js
+++ app.js
@@ -46,3 +46,55 @@ setInterval(() => {
   cookieCount += cps;
   updateDisplay();
 }, 1000);
+
+// un pas 8: upgrade si facut div shop sa le afisez - fetch api
+
+async function fetchUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  const upgrades = await response.json();
+  return upgrades;
+}
+
+//chemat fetchUpgrades + afisat + buy button
+
+fetchUpgrades().then((upgrades) => {
+  const shopDiv = document.getElementById("shop");
+
+  upgrades.forEach((upgrade) => {
+    //container pt fiecare upgrade  //blocuri html
+    const upgradeDiv = document.createElement("div");
+    upgradeDiv.classList.add("upgrade");
+
+    const name = document.createElement("h3"); //nume upgrade
+    name.textContent = upgrade.name;
+
+    const cost = document.createElement("p"); //cost upgrade
+    cost.textContent = `Cost: ${upgrade.cost}`;
+
+    const increase = document.createElement("p"); //cat adauga la  c pe secunda
+    increase.textContent = `+${upgrade.increase} CPS`;
+
+    const buyButton = document.createElement("button"); // buy button
+    buyButton.textContent = "Buy";
+
+    buyButton.addEventListener("click", () => {
+      //click pt a cumpara
+      if (cookieCount >= upgrade.cost) {
+        cookieCount -= upgrade.cost;
+        cps += upgrade.increase;
+        updateDisplay(); //actualizez val in pag si salvez in local storage
+      } else {
+        alert("Not enough cookies!");
+      }
+    });
+    // pun toate in div-ul upgrade-ului
+    upgradeDiv.appendChild(name);
+    upgradeDiv.appendChild(cost);
+    upgradeDiv.appendChild(increase);
+    upgradeDiv.appendChild(buyButton);
+    // pun upgrade-ul in shop
+    shopDiv.appendChild(upgradeDiv);
+  });
+});
